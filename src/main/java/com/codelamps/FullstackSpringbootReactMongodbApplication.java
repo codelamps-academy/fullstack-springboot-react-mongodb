f53ff9e1d3cc1a04ffa373a55fc0cc0cad81d389
Codelamps <andrerizaldib@gmail.com>
2023-07-16 02:25:03 +0700

    create fullstack-springboot-react-mongodb

---
+++ src/main/java/com/codelamps/FullstackSpringbootReactMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.codelamps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackSpringbootReactMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackSpringbootReactMongodbApplication.class, args);
+    }
+
+}
